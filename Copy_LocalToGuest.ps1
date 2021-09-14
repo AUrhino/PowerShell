8106723877f9d0c44f68d88bd5e043f167c7b248
Ryan <ryangillan@gmail.com>
2021-09-14 17:38:59 +1000

    Update Copy_LocalToGuest.ps1

--- Copy_LocalToGuest.ps1
+++ Copy_LocalToGuest.ps1
@@ -11,12 +11,9 @@ $DstPath = Read-Host -Prompt 'Prompt for the destination'
 Foreach ($VMName in $VMs) {
   $VM = Get-VM $VMName
 
-#Define file information. Include File Name, Parameters, Source and Destination
-  $SrcPath = "c:\"
-  $DstPath = "c:\temp\"
+  #Define file information. Include File Name, Parameters, Source and Destination
   $Fullpath = $SrcPath + $File
 
   Write-Host Copying $Fullpath to $VMName -ForegroundColor Cyan
   Copy-VMGuestFile -VM $VM -Source $Fullpath -Destination $DstPath -LocalToGuest -GuestCredential $Cred -Force
-
 }
